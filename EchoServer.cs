971740d449162246f07a8f5ac73a3962341226a4
svitlanaena15-cmd <svitlanaena15@gmail.com>
2026-01-16 20:40:07 +0100

    fix infinity loop echo server

--- EchoServer.cs
+++ EchoServer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace labora1
@@ -10,6 +11,7 @@ namespace labora1
     {
         private readonly int port;
         private TcpListener? listener;
+        private readonly CancellationTokenSource cts = new(); //
 
         public EchoServer(int port)
         {
@@ -21,36 +23,45 @@ namespace labora1
             listener = new TcpListener(IPAddress.Loopback, port);
             listener.Start();
 
-            while (true)
+            try
             {
-                var client = await listener.AcceptTcpClientAsync();
-                _ = HandleClientAsync(client); 
+                while (!cts.IsCancellationRequested)
+                {
+                    var client = await listener.AcceptTcpClientAsync();
+                    _ = HandleClientAsync(client);
+                }
+            }
+            catch (SocketException)
+            {
             }
         }
 
         public void Stop()
         {
+            cts.Cancel();
             listener?.Stop();
         }
 
         public async Task<string> ProcessMessageAsync(string message)
         {
-            await Task.Delay(1); // імітує асинхронну роботу :)
+            await Task.Delay(1); // імітація асинхронної роботи :))
             return message;
         }
 
-        // обробка ТСР внутрішньо
+        // обробка TCP клієнта
         private async Task HandleClientAsync(TcpClient client)
         {
             using var stream = client.GetStream();
+
             byte[] buffer = new byte[1024];
             int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
             string received = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-
             string response = await ProcessMessageAsync(received);
-            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
 
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
             await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+
             client.Close();
         }
     }
